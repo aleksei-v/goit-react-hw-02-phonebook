fbb767588d8c8034d0ed2e0914d8f9e16dbe6986
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-09-24 17:58:30 +0300

    add opportunity to create contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,29 @@
 // import { Box } from "./Box";
 import React, { Component } from "react";
+import { Form } from "./Form/Form";
 
 export class App extends Component {
 
     state = {
         contacts: [],
-        name: ''
     }
-
-    handleInputChange = evt => {
-        this.setState({
-            [evt.currentTarget.name]: evt.currentTarget.value,
-        });
+    receivedData = data => {
+        this.setState((prevState) => ({
+            contacts: [...prevState.contacts, data]
+        }))
     };
-    onClickSubmit = evt => {
-      evt.preventDefault();
-      
-      this.state.contacts.push(evt.currentTarget.value)
-      console.log(this.state)
-
-      this.resetForm();
-    }
-  resetForm = () => {
-    this.setState({
-      name: "",
-    });
-  };
-
+    
     render() {
         return (
             <>
-                <h1>Phonebook</h1>
-                <form onSubmit={this.onClickSubmit}>
-                    <label>Name
-                        <input
-                  type="text"
-                            name="name"
-                            value={this.state.name}
-                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                            required
-                            onChange={this.handleInputChange}
-                        />
-                    </label>
-                    <button
-                        type="submit"
-                    >
-                        Add contact
-                    </button>
-                </form>                      
+                <Form onSubmit={this.receivedData} />
+                {this.state.contacts.map((contact => {
+                    return (
+                        <li key={contact}>
+                            {contact}
+                        </li>
+                    )
+                }))}
             </>
-    )
-}
-
-}
+        )
+    }};
